dbe829a514516a417315672bd0c724f985132331
AndresCaste2001 <143757955+AndresCaste2001@users.noreply.github.com>
2024-04-18 17:10:09 -0500

    feat: :tada: Ejercicios Json

---
+++ js/main.js
@@ -0,0 +1,6 @@
+import prompt from 'async-prompt'
+import {sumarRango} from "./module/ejericicioSuma.js"
+
+let num1 = Number(await prompt("Ingrese el numero 1 \n"))
+let num2 = Number(await prompt("Ingrese el numero 2 \n"))
+console.log(sumarRango(num1,num2))
